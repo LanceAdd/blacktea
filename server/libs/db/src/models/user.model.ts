a1d0961e1babad4c8258e7678fc4c8e0d9d182f3
lanceadd <1196661499@qq.com>
2019-10-28 13:05:47 +0800

    curd and db

---
+++ server/libs/db/src/models/user.model.ts
@@ -0,0 +1,13 @@
+import { prop } from '@typegoose/typegoose';
+import { ApiModelProperty } from '@nestjs/swagger';
+
+export class User {
+    @ApiModelProperty({ description: '用户名', example: 'user1' })
+    @prop()
+    username: string;
+
+    @ApiModelProperty({ description: '密码', example: 'pass1' })
+    @prop()
+    password: string;
+
+}
